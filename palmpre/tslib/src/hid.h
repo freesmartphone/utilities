7137c7620ae99c6804ee733124b28ef43b39e2cf
Simon Busch <morphis@gravedo.de>
2010-03-16 10:05:16 +0100

    palmpre/tslib: add header file for libhid

---
+++ palmpre/tslib/src/hid.h
@@ -0,0 +1,22 @@
+
+#ifndef _HID_H_
+#define _HID_H_
+
+typedef int hid_plugin_settings_t;
+typedef int hid_handle_t;
+
+int HidAllocPluginSettings
+	(const char *config_file, hid_plugin_settings_t *settings, int *num_plugins);
+void HidFreePluginSettings
+	(hid_plugin_settings_t **settings, int num_plugins);
+int HidInitPluginTransport
+	(const char *plugin_name, hid_plugin_settings_t *settings, int num_plugins, hid_handle_t **handle);
+int HidHandleGetFd
+	(hid_handle_t *handle);
+int HidHandleReadInputEvent
+	(hid_handle_t* handle, struct input_event *event, int max_events);
+void HidDestroyPluginTransport
+	(hid_handle_t **handle);
+
+#endif
+	
