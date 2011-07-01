43febefbe57c1a21f53edc5c6990f28bc42c93ac
Simon Busch <morphis@gravedo.de>
2011-07-01 22:29:30 +0200

    fso-installer: add files necessary for accelerated graphics (OpenGL) to setup-extra.sh

--- palmpre/fso-installer/scripts/setup-extra.sh
+++ palmpre/fso-installer/scripts/setup-extra.sh
@@ -12,6 +12,24 @@ WEBOS_FILES="\
 	etc/camd/vx6852-once-0.10.bin \
 	etc/camd/vx6852-once-1.20.bin \
 	etc/camd/vx6852-once-2.21.bin \
+	usr/lib/libEGL.so
+	usr/lib/libsrv_um.so
+	usr/lib/libIMGegl.so
+	usr/lib/libpvrPVR2D_LINUXFBWSEGL.so
+	usr/lib/libpvrPVR2D_BLITWSEGL.so
+	usr/lib/libOpenVGU.so
+	usr/lib/libsrv_init.so
+	usr/lib/libPVRScopeServices.so
+	usr/lib/libpvrPVR2D_FRONTWSEGL.so
+	usr/lib/libOpenVG.so
+	usr/lib/libGLES_CM.so
+	usr/lib/libpvrPVR2D_FLIPWSEGL.so
+	usr/lib/libpvr2d.so
+	usr/lib/libGLESv2.so
+	usr/lib/libews.so
+	usr/lib/libglslcompiler.so
+	usr/lib/libpvrEWS_WSEGL.so
+	usr/bin/pvrsrvinit
 "
 
 for file in $WEBOS_FILES ; do
