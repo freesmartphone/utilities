305509770f4eff0b7e4023102bb2e2620d9a7da0
Simon Busch <morphis@gravedo.de>
2011-07-01 22:26:29 +0200

    fso-installer: remove wifi stuff from setup-extra.sh script

--- palmpre/fso-installer/scripts/setup-extra.sh
+++ palmpre/fso-installer/scripts/setup-extra.sh
@@ -6,12 +6,6 @@ if [ ! -d build/unpacked-doctor/rootfs ] ; then
 fi
 
 WEBOS_FILES="\
-	lib/firmware/helper_sd.bin \
-	lib/firmware/sd8686.bin \
-	lib/firmware/mfg_sd8686.bin \
-	lib/firmware/sd8686_ap.bin \
-	lib/modules/2.6.24-palm-joplin-3430/kernel/net/wifi/uap8xxx.ko \
-	lib/modules/2.6.24-palm-joplin-3430/kernel/net/wifi/sd8xxx.ko \
 	etc/camd/smia_ed.cfg \
 	etc/camd/vx6852-load-firmware \
 	etc/camd/vx6852-once-0.00.bin \
@@ -21,13 +15,6 @@ WEBOS_FILES="\
 "
 
 for file in $WEBOS_FILES ; do
-	if [ $file == "lib/firmware/sd8686.bin" ] ; then
-		# Simple quirk for WiFi firmware as we already have a firmware for sd8686
-		# installed with the same name
-		mkdir -p extra/lib/firmware
-		cp build/unpacked-doctor/rootfs/$file extra/lib/firmware/sd8686-palm.bin
-	else
-		mkdir -p extra/`dirname $file`
-		cp build/unpacked-doctor/rootfs/$file extra/$file
-	fi
+	mkdir -p extra/`dirname $file`
+	cp build/unpacked-doctor/rootfs/$file extra/$file
 done
